491cb59a5183c2a7803faa0a0ab4f5464a7c05ef
Teodor-Todorov1 <todorvt@students.softuni.bg>
2023-12-09 17:56:47 +0200

    Added about page

--- my_app/core/core.py
+++ my_app/core/core.py
@@ -10,3 +10,10 @@ core_bp = Blueprint(
     static_folder='static',
     static_url_path='/core/static/'
 )
+
+@core_bp.route("/about")
+def about():
+    if session.get("is_logged_in"):
+        return render_template("about.html")
+    else:
+        return redirect(url_for('registration_bp.login'))
